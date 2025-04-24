ef6ee4eabba9eb54ce814893c17323012dcfce90
Tahasiraj1 <tahasiraj242@gmail.com>
2025-04-25 01:33:20 +0500

    Add a new example demonstrating Method Resolution Order (MRO) and diamond inheritance in Python with classes A, B, C, and D, including method overrides and MRO output.

---
+++ 15. Method Resolution Order (MRO) and Diamond Inheritance/main.py
@@ -0,0 +1,40 @@
+class A:
+    def show(self):
+        print("Show A")
+
+class B(A):
+    def show(self):
+        print("Show B")
+
+class C(A):
+    def show(self):
+        print("Show C")
+
+class D(B, C):
+    pass
+
+d = D()
+print("\n")
+print(d.show())
+
+# Prints the method resolution order of D.
+print(D.mro()) # Show B
+# [<class '__main__.D'>, <class '__main__.B'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>]
+
+# So, when we call d.show(), Python looks for show() in this order:
+
+#       A
+#     /   \
+#    B     C
+#     \   /
+#       D
+
+# D → no method defined here.
+
+# B → ✅ show() found here, so it executes this.
+
+# C → skipped because B already resolved it.
+
+# A → not checked since already resolved.
+
+# object → not needed.
